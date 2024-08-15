1718edc3b677c11e2992cf1e167f56014487af72
Abdul Amin Khan <abdulaminkhanpaypal@gmail.com>
2024-08-15 20:58:35 +0530

    Connect to Cloudinary & create FileUploder.js File

--- backend/index.js
+++ backend/index.js
@@ -4,3 +4,12 @@ const bodyParser = require('body-parser');
 require('dotenv').config();
  
 const PORT = process.env.PORT || 8080;
+
+require('./Models/db');
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT: ${PORT}`);
+});
